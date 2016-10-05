85cc2358966ec808e72881c0c1b23c77c543b115
desireeleavell <desireeleavell@gmail.com>
2016-10-04 20:26:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $("readmore").click(function(){
+        $("result").hide();
+    });
+    $("readmore").click(function(){
+        $("result").show();
+    });
+});
